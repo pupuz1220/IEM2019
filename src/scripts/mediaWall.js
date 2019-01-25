0b53c6d0800fc58a7af3a5b4dbb8653818666fd0
pupuz1220 <44030784+pupuz1220@users.noreply.github.com>
2019-01-25 17:03:04 +0800

    Add files via upload

---
+++ src/scripts/mediaWall.js
@@ -0,0 +1,7 @@
+<script id="sprinklr-embed-loader-3623-ZW1iZWQ6NWJhY2I4MTgxNjIzYWU3MDhiYjA3N2I3" type="text/javascript" src="https://gallery.sprinklr.com/clients/3623/embeds/ZW1iZWQ6NWJhY2I4MTgxNjIzYWU3MDhiYjA3N2I3/script.js"></script>
+
+if (window.ActiveXObject || "ActiveXObject" in window){
+
+    $('.ProductVideo').css('display','block');
+    $('.ProductTableCell').css('display','block');
+}
